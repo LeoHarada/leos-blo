363416ff302a334d9aa6c42d5e6b52a0cbe7db58
Leo Harada <leotharada@gmail.com>
2024-01-22 23:53:42 -0800

    Initiated new Next app project with TailwindCSS.

---
+++ app/layout/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Header = (props: Props) => {
+    return (
+        <div className="px-2 py-4 border-b">
+            <div className="text-4xl">LOGO1</div>
+        </div>
+    );
+};
+
+export default Header;
