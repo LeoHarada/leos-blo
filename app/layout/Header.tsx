36a54f9de0205788dbad7af60ea268a95f4c86f4
Leo Harada <leotharada@gmail.com>
2024-01-24 13:48:33 -0800

    Created single page for blog post.

--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 type Props = {};
 
 const Header = (props: Props) => {
     return (
         <div className="px-2 py-4 border-b flex justify-between">
-            <div className="text-4xl px-2 py-4">LOGO</div>
+            <Link href={"/"} className="text-4xl px-2 py-4">
+                LOGO
+            </Link>
             <button className="text-white bg-black px-2 py-4">Login</button>
         </div>
     );
