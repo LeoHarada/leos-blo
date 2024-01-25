1339f29725f34f53214a37219a6df35f8fa20590
Leo Harada <leotharada@gmail.com>
2024-01-24 18:44:01 -0800

    Created form for new Post submission.

---
+++ app/blog/new/NewBlogForm.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { useSession } from "next-auth/react";
+import { createPost } from "@/app/actions/publishPost";
+
+type Props = {};
+
+const NewBlogForm = (props: Props) => {
+    const { data: session } = useSession();
+
+    const [title, setTitle] = useState<string>("");
+    const [content, setContent] = useState<string>("");
+
+    if (!session) return <div>You must be signed in to post</div>;
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {};
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    name="title"
+                />
+                <textarea
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    name="content"
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default NewBlogForm;
