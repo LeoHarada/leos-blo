f199ddeb59285d17c4e476cfeddb9e8c34c2be02
Leo Harada <leotharada@gmail.com>
2024-01-24 17:28:20 -0800

    Added oAuth Signup/Login through GitHub.

--- app/layout/Header.tsx
+++ app/layout/Header.tsx
@@ -1,16 +1,21 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { Button as AuthButton } from "../auth/Button";
+import { SessionProvider } from "next-auth/react";
 
 type Props = {};
 
 const Header = (props: Props) => {
     return (
-        <div className="px-2 py-4 border-b flex justify-between">
-            <Link href={"/"} className="text-4xl px-2 py-4">
-                LOGO
-            </Link>
-            <button className="text-white bg-black px-2 py-4">Login</button>
-        </div>
+        <SessionProvider>
+            <div className="px-2 py-4 border-b flex justify-between">
+                <Link href={"/"} className="text-4xl px-2 py-4">
+                    LOGO
+                </Link>
+                <AuthButton />
+            </div>
+        </SessionProvider>
     );
 };
 
