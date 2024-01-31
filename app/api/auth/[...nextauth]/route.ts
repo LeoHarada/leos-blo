98f3574f93fb790669f921263dbe55c3acc927bd
Leo Harada <leotharada@gmail.com>
2024-01-30 18:03:09 -0800

    fixing bugs part 2.:

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,11 +2,12 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import type { Adapter } from "next-auth/adapters";
 
 const prisma = new PrismaClient();
 
 const handler = NextAuth({
-    adapter: PrismaAdapter(prisma),
+    adapter: PrismaAdapter(prisma) as Adapter,
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID as string,
